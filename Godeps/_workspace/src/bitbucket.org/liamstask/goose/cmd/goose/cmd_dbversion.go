5001a8e9f0467295db77938ba4b0a1043c1b6fe9
Daniel Brotsky <dev@brotsky.com>
2016-02-28 12:49:49 -0800

    get ready to use a relational db
    1. add goose to the dependencies
    2. create a local dev psql database
    3. add a Heroku free psql database

---
+++ Godeps/_workspace/src/bitbucket.org/liamstask/goose/cmd/goose/cmd_dbversion.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ancientHacker/susen.go/Godeps/_workspace/src/bitbucket.org/liamstask/goose/lib/goose"
+	"log"
+)
+
+var dbVersionCmd = &Command{
+	Name:    "dbversion",
+	Usage:   "",
+	Summary: "Print the current version of the database",
+	Help:    `dbversion extended help here...`,
+	Run:     dbVersionRun,
+}
+
+func dbVersionRun(cmd *Command, args ...string) {
+	conf, err := dbConfFromFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	current, err := goose.GetDBVersion(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("goose: dbversion %v\n", current)
+}
