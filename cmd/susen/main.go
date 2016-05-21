95868ea0c0891bdaec70c82c33cf133dcb4ceb0f
Daniel Brotsky <dev@brotsky.com>
2016-05-20 17:51:30 -0700

    Update web server to use new components.
    Use the updated templates and the separated storage component.

--- cmd/susen/main.go
+++ cmd/susen/main.go
@@ -21,6 +21,7 @@ package main
 import (
 	"fmt"
 	"github.com/ancientHacker/susen.go/client"
+	"github.com/ancientHacker/susen.go/puzzle"
 	"github.com/ancientHacker/susen.go/storage"
 	"log"
 	"net/http"
@@ -40,9 +41,12 @@ func main() {
 		shutdown(startupFailureShutdown)
 	}
 	// storage initialization
-	if err := storage.Connect(); err != nil {
+	if cacheId, databaseId, err := storage.Connect(); err != nil {
 		log.Printf("Error during storage initialization: %v", err)
 		shutdown(startupFailureShutdown)
+	} else {
+		log.Printf("Connected to cache at %q", cacheId)
+		log.Printf("Connected to database at %q", databaseId)
 	}
 
 	// port sensing
@@ -70,65 +74,124 @@ func main() {
 
 /*
 
+sessions
+
+*/
+
+type session struct {
+	sid string           // session ID
+	ss  *storage.Session // underlying storage session
+}
+
+// working puzzle state
+func (s *session) puzzle() *puzzle.Puzzle {
+	return s.ss.Puzzle
+}
+
+// ID of working puzzle
+func (s *session) pid() string {
+	return s.ss.Info.PuzzleId
+}
+
+// name of working puzzle
+func (s *session) name() string {
+	return s.ss.Info.Name
+}
+
+// step count of working puzzle
+func (s *session) step() int {
+	return len(s.ss.Info.Choices) + 1
+}
+
+/*
+
 request handlers
 
 */
 
-var apiEndpointRegexp = regexp.MustCompile("^/+api/+([a-z]+)/*$")
-
-type userSession struct {
-	storage.Session
-}
+// endpoint regular expressions
+var (
+	apiEndpointRegexp    = regexp.MustCompile("^/+api/+([a-z]+)/*$")
+	selectEndpointRegexp = regexp.MustCompile("^/+(reset|select)/+([a-z0-9-]+)/*$")
+)
 
 func serveHttp(w http.ResponseWriter, r *http.Request) {
+	// session selection
+	var s *session
+
+	// runtime error handling
 	defer func() {
 		if err := recover(); err != nil {
+			if s == nil || s.sid == "" {
+				log.Printf("Error getting session cookie: %v", err)
+			} else if s.ss != nil {
+				log.Printf("Error in session %s:%q step %d: %v", s.sid, s.name(), s.step(), err)
+			} else {
+				log.Printf("Error in session %s: %v", s.sid, err)
+			}
 			errorHandler(err, w, r)
 		}
 	}()
 
+	s = &session{sid: getCookie(w, r)}
 	if client.StaticHandler(w, r) {
 		return
 	}
-	session := sessionSelect(w, r)
-	session.rootHandler(w, r)
+	s.load(w, r)
+	s.rootHandler(w, r)
 }
 
-func (session *userSession) apiHandler(w http.ResponseWriter, r *http.Request) {
+func (s *session) rootHandler(w http.ResponseWriter, r *http.Request) {
+	if test, _ := regexp.MatchString("^/+api/?", r.URL.Path); test {
+		s.apiHandler(w, r)
+	} else if test, _ = regexp.MatchString("^/+solver/?", r.URL.Path); test {
+		s.solverHandler(w, r)
+	} else if test, _ = regexp.MatchString("^/+home/?", r.URL.Path); test {
+		s.homeHandler(w, r)
+	} else {
+		http.Redirect(w, r, "/home/", http.StatusFound)
+		log.Printf("Redirected to home page on request for %q", r.URL.Path)
+	}
+}
+
+func (s *session) apiHandler(w http.ResponseWriter, r *http.Request) {
 	sendState := func() {
-		session.Puzzle.StateHandler(w, r)
-		log.Printf("Returned current state for %s:%q step %d", session.SID, session.PID, session.Step)
+		s.puzzle().StateHandler(w, r)
+		log.Printf("Returned current state for %s:%q step %d", s.sid, s.name(), s.step())
 	}
 	sendSummary := func() {
-		session.Puzzle.SummaryHandler(w, r)
-		log.Printf("Returned current summary for %s:%q step %d", session.SID, session.PID, session.Step)
+		s.puzzle().SummaryHandler(w, r)
+		log.Printf("Returned current summary for %s:%q step %d", s.sid, s.name(), s.step())
 	}
 	sendNotAllowed := func() {
 		http.Error(w, apiEndpointUnknown(r.URL.Path), http.StatusMethodNotAllowed)
-		log.Printf("Endpoint %q cannot accept %s: returned a MethodNotAllowed error.", r.URL.Path, r.Method)
+		log.Printf("Endpoint %q cannot accept %s: returned a MethodNotAllowed error.",
+			r.URL.Path, r.Method)
 	}
 	sendNotFound := func() {
 		http.Error(w, apiEndpointUnknown(r.URL.Path), http.StatusNotFound)
-		log.Printf("Endpoint %q unknown: returned a NotFound error.", r.URL.Path)
+		log.Printf("Uknown endpoint %q: returned a NotFound error.", r.URL.Path)
 	}
 
 	matches := apiEndpointRegexp.FindStringSubmatch(r.URL.Path)
 	if matches == nil {
-		http.Error(w, apiEndpointUnknown(r.URL.Path), http.StatusNotFound)
-		log.Printf("Unknown endpoint %q: returned a NotFound error.", r.URL.Path)
+		sendNotFound()
 		return
 	}
-	switch matches[1] {
+	switch strings.ToLower(matches[1]) {
 	case "reset":
 		if r.Method == "GET" {
-			session.StartPuzzle("")
+			s.ss.RemoveAllSteps()
 			sendState()
 		} else {
 			sendNotAllowed()
 		}
 	case "back":
 		if r.Method == "GET" {
-			session.RemoveStep()
+			if s.step() > 1 {
+				s.ss.RemoveStep()
+				log.Printf("Reverted session %s:%q to step %d.", s.sid, s.name(), s.step()-1)
+			}
 			sendState()
 		} else {
 			sendNotAllowed()
@@ -147,14 +210,22 @@ func (session *userSession) apiHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	case "assign":
 		if r.Method == "POST" {
-			update, e := session.Puzzle.AssignHandler(w, r)
-			if e != nil {
-				log.Printf("Assign to %s:%q step %d failed: %v", session.SID, session.PID, session.Step, e)
+			choice, update, err := s.puzzle().AssignHandler(w, r)
+			if update == nil {
+				log.Printf("Assign of %+v at %s:%q step %d failed: %v",
+					*choice, s.sid, s.name(), s.step(), err)
 			} else {
-				session.AddStep()
-				if update.Errors != nil {
-					log.Printf("Assign to %s:%q gave errors; step %d is unsolvable.",
-						session.SID, session.PID, session.Step)
+				if len(update.Errors) > 0 {
+					log.Printf("Assign of %+v at %s:%q step %d made puzzle unsolvable.",
+						choice, s.sid, s.name(), s.step())
+				} else {
+					log.Printf("Assign of %+v at %v:%q step %d left puzzle solvable",
+						choice, s.sid, s.name(), s.step())
+				}
+				s.ss.AddStep(*choice)
+				if err != nil {
+					log.Printf("WARNING: Result of assign at %v:%q step %d failed to encode!",
+						s.sid, s.name(), s.step())
 				}
 			}
 		} else {
@@ -165,49 +236,29 @@ func (session *userSession) apiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (session *userSession) solverHandler(w http.ResponseWriter, r *http.Request) {
-	body := client.SolverPage(session.SID, session.PID, session.Summary)
+func (s *session) solverHandler(w http.ResponseWriter, r *http.Request) {
+	summary, err := s.puzzle().Summary()
+	if err != nil {
+		panic(fmt.Errorf("Failed to create summary for puzzle: %v", err))
+	}
+	body := client.SolverPage(s.sid, s.ss.Info, summary.Values)
 	hs := w.Header()
 	hs.Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(body))
-	log.Printf("Returned solver page for %s:%q step %d.", session.SID, session.PID, session.Step)
+	log.Printf("Returned solver page for %s:%q step %d.", s.sid, s.name(), s.step())
 }
 
-func (session *userSession) homeHandler(w http.ResponseWriter, r *http.Request) {
-	var others []string
-	for k := range storage.CommonSummaries() {
-		if k != session.PID {
-			others = append(others, k)
-		}
-	}
-	sort.Strings(others)
-	body := client.HomePage(session.SID, session.PID, others)
+func (s *session) homeHandler(w http.ResponseWriter, r *http.Request) {
+	infos := s.ss.GetInactivePuzzles()
+	sort.Sort(storage.ByLatestView(infos))
+	body := client.HomePage(s.sid, s.ss.Info, infos)
 	hs := w.Header()
 	hs.Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(body))
 	log.Printf("Returned home page for %s:%q step %d.",
-		session.SID, session.PID, session.Step)
-}
-
-func (session *userSession) rootHandler(w http.ResponseWriter, r *http.Request) {
-	switch {
-	case strings.HasPrefix(r.URL.Path, "/reset/"):
-		http.Redirect(w, r, "/solver/", http.StatusFound)
-		log.Printf("Redirected to solver page for %s:%q step %d.",
-			session.SID, session.PID, session.Step)
-	case strings.HasPrefix(r.URL.Path, "/api/"):
-		session.apiHandler(w, r)
-	case strings.HasPrefix(r.URL.Path, "/solver/"):
-		session.solverHandler(w, r)
-	case strings.HasPrefix(r.URL.Path, "/home/"):
-		session.homeHandler(w, r)
-	default:
-		http.Redirect(w, r, "/home/", http.StatusFound)
-		log.Printf("Redirected to home page for %s:%q step %d.",
-			session.SID, session.PID, session.Step)
-	}
+		s.sid, s.name(), s.step())
 }
 
 func errorHandler(err interface{}, w http.ResponseWriter, r *http.Request) {
@@ -235,9 +286,11 @@ session handling
 // the session ID in the cookie).  These are the values shared
 // among all our cookies.
 const (
-	cookieNameBase = "susenID"
-	cookiePath     = "/"
-	cookieMaxAge   = 3600 * 24 * 365 * 10 // 10 years
+	cookieNameBase   = "susenID"
+	cookieAgeBase    = "susenDate"
+	cookiePath       = "/"
+	cookieMaxAge     = 3600 * 24 * 365 // 1 year
+	cookieRefreshAge = 3600 * 24 * 1   // 1 day
 )
 
 var (
@@ -260,50 +313,67 @@ func getCookie(w http.ResponseWriter, r *http.Request) string {
 		proto = herokuProtocol
 	}
 
+	// helpers for getting and setting cookies
+	getCookies := func() (id string, age bool) {
+		idName, ageName := cookieNameBase+"-"+proto, cookieAgeBase+"-"+proto
+		if sc, e := r.Cookie(idName); e == nil && sc.Value != "" {
+			id = sc.Value
+		}
+		if sc, e := r.Cookie(ageName); e == nil && sc.Value != "" {
+			age = true
+		}
+		return
+	}
+	setCookies := func(id string) {
+		idName, ageName := cookieNameBase+"-"+proto, cookieAgeBase+"-"+proto
+		http.SetCookie(w, &http.Cookie{
+			Name: idName, Value: id, Path: cookiePath, MaxAge: cookieMaxAge})
+		now := time.Now().Format(time.RFC822)
+		http.SetCookie(w, &http.Cookie{
+			Name: ageName, Value: now, Path: cookiePath, MaxAge: cookieRefreshAge})
+	}
+
 	// check for an existing cookie whose name matches the protocol
-	cookieName := cookieNameBase + "-" + proto
-	if sc, e := r.Cookie(cookieName); e == nil && sc.Value != "" {
-		return sc.Value
+	id, age := getCookies()
+	if id != "" {
+		// refresh both cookies if the date cookie has expired
+		if !age {
+			setCookies(id)
+		}
+		return id
 	}
 
 	// no session cookie: start a new session with a new ID
 	// poor man's UUID for the session in local mode: time since startup.
 	sid := strconv.FormatInt(int64(time.Now().Sub(startTime)), 36)
-	// if we're on an infrastructure, we use the request ID
+	// if we're on Heroku infrastructure, we use the request ID
 	if requestID := r.Header.Get("X-Request-ID"); requestID != "" {
 		sid = requestID
 	}
 	log.Printf("No session cookie found, created new session ID %q", sid)
-	sc := &http.Cookie{Name: cookieName, Value: sid, Path: cookiePath, MaxAge: cookieMaxAge}
-	http.SetCookie(w, sc)
+	setCookies(sid)
 	return sid
 }
 
-// sessionSelect: find or create the session for the current connection.
-func sessionSelect(w http.ResponseWriter, r *http.Request) *userSession {
-	// check to see if this is a force reset of the session
-	forceReset, resetID := false, ""
-	if strings.HasPrefix(r.URL.Path, "/reset/") {
-		forceReset = true
-		resetID = r.URL.Path[len("/reset/"):]
-	}
-	id := getCookie(w, r)
-	// create an in-memory session with this cookie
-	session := &userSession{storage.Session{SID: id, Created: time.Now().Format(time.RFC3339)}}
-	// load session from storage if possible, otherwise just initialize it
-	if session.Lookup() {
-		log.Printf("Found session %v, puzzle %q, on step %d.", session.SID, session.PID, session.Step)
-		if forceReset {
-			session.StartPuzzle(resetID)
-		} else {
-			session.LoadStep()
+// load: load the session for the current connection.
+func (s *session) load(w http.ResponseWriter, r *http.Request) {
+	// get the stored session
+	s.ss = storage.LoadSession(s.sid)
+
+	// reset the session if requested
+	matches := selectEndpointRegexp.FindStringSubmatch(r.URL.Path)
+	log.Printf("%s matches: %v", r.URL.Path, matches)
+	if matches != nil {
+		if len(matches[2]) > 0 {
+			s.ss.SelectPuzzle(matches[2])
+			log.Printf("Selected session %v puzzle %q at step %d.", s.sid, s.name(), s.step())
 		}
-	} else if forceReset {
-		session.StartPuzzle(resetID)
-	} else {
-		session.StartPuzzle(storage.DefaultPuzzleID())
+		if matches[1] == "reset" {
+			s.ss.RemoveAllSteps()
+			log.Printf("Reset session %v puzzle %q to step %d", s.sid, s.name(), s.step())
+		}
+		r.URL.Path = "/solver/"
 	}
-	return session
 }
 
 /*
